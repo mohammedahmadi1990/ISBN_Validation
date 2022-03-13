c633a570bbfe9f8ca523a0b5c60413004bf036d0
Mohammed <mohammed.ahmadi1990@gmail.com>
2022-03-13 16:31:00 +0300

    Parsing ISBN
    codes are being parsed.

--- isbn.cob
+++ isbn.cob
@@ -11,19 +11,30 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01 i PIC 99 VALUE 0.
        01 isbnStruct.
            05 inputFileName PIC x(20).
-           05 isbnArray PIC x(10) OCCURS 10 TIMES INDEXED BY i.
+           05 isbnArray PIC x(10) OCCURS 10 TIMES.
+           05 isbnMessage OCCURS 10 TIMES.
+               10 iMessage PIC x(50) VALUES SPACES.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
+
            DISPLAY
            "..:|Welcome to ISBN Validation Application in COBOL |:..".
-           CALL "readISBN" USING BY REFERENCE isbnStruct.
 
-           DISPLAY isbnArray(2).
+           CALL "readISBN" USING BY REFERENCE isbnStruct END-CALL.
+           CALL "isValid" USING BY REFERENCE isbnStruct END-CALL.
+           CALL "checkSUM" USING BY REFERENCE isbnStruct END-CALL.
+
+           SET i TO 1.
+           PERFORM UNTIL i > 10
+               DISPLAY isbnArray(i) " " isbnMessage(i)
+               COMPUTE i = i + 1
+           END-PERFORM.
+
            STOP RUN.
-       END PROGRAM isbn.
 
       ******************************************************************
       ***************************** readISBN ***************************
@@ -45,15 +56,7 @@
 
        WORKING-STORAGE SECTION.
        01  fileStatus PIC x(02).
-       01  fileReadStatus PIC x(1).
-           88 endOfFile VALUE 'Y'.
-           88 notEndOfFile VALUE 'N'.
-       01  requests PIC 9(1) VALUE ZERO.
-
-       77 n          pic 99.
        77 feof       pic A(1).
-       77 isbnVar PIC x(10).
-       01 r-isbnData PIC x(10).
 
        LINKAGE SECTION.
        01 isbnStruct.
@@ -72,14 +75,13 @@
                DISPLAY "File not found!"
                GO TO MAIN-PROCEDURE
            WHEN fileStatus = "00"
-               SET i TO 0
+               SET i TO 1
                PERFORM UNTIL feof='Y'
-                   READ inputfile INTO r-isbnData
+                   READ inputfile INTO isbnArray OF isbnStruct(i)
                    AT END
                        MOVE 'Y' TO feof
                    NOT AT END
                        COMPUTE i = i + 1
-                       MOVE r-isbnData TO isbnArray OF isbnStruct(i)
                    END-READ
                END-PERFORM
            WHEN OTHER
@@ -89,3 +91,73 @@
            GOBACK.
 
        END PROGRAM readISBN.
+
+      ******************************************************************
+      ***************************** isValid ****************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. isValid.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT section.
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       WORKING-STORAGE SECTION.
+
+       LINKAGE SECTION.
+       01 isbnStruct.
+           05 inputFileName PIC x(20).
+           05 isbnArray PIC x(10) OCCURS 10 TIMES INDEXED BY i.
+           05 isbnMessage OCCURS 10 TIMES.
+               10 iMessage PIC x(50) VALUES SPACES.
+
+       PROCEDURE DIVISION USING isbnStruct.
+
+       A-PARA.
+           SET i TO 1.
+           PERFORM B-PARA WITH TEST AFTER UNTIL i>3.
+           STOP RUN.
+       B-PARA.
+           IF isbnArray(i)(1:1) EQUALS TO "0" THEN
+               DISPLAY "xxxxx"
+               MOVE 'leading zero' TO isbnMessage(i)
+           END-IF
+           IF isbnArray(i)(10:1) EQUALS TO "0" THEN
+               MOVE "trailing zero" TO isbnMessage(i)
+           END-IF
+           IF isbnArray(i)(10:1) EQUALS TO "X" THEN
+               MOVE "trailing uppercase X" TO isbnMessage(i)
+           END-IF
+           IF isbnArray(i)(10:1) EQUALS TO "x" THEN
+               MOVE "trailing lowercase X" TO isbnMessage(i)
+           END-IF
+           COMPUTE i = i + 1
+
+       EXIT PROGRAM.
+       END PROGRAM isValid.
+
+      ******************************************************************
+      ***************************** checkSUM ****************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. checkSUM.
+
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT section.
+
+       DATA DIVISION.
+       FILE SECTION.
+
+       WORKING-STORAGE SECTION.
+
+       LINKAGE SECTION.
+       01 isbnStruct.
+           05 inputFileName PIC x(20).
+           05 isbnArray PIC x(10) OCCURS 10 TIMES INDEXED BY i.
+
+       PROCEDURE DIVISION USING isbnStruct.
+       MAIN-PROCEDURE.
+
+
+           GOBACK.
+       END PROGRAM checkSUM.
